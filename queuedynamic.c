43d4d46ac41bf5b125b90f821e085da49c589eb6
lilshetty <120896604+lilshetty@users.noreply.github.com>
2023-04-12 01:49:36 +0530

    Update queuedynamic.c

--- queuedynamic.c
+++ queuedynamic.c
@@ -59,10 +59,6 @@ int main(){
     enqueue(&q, 1); 
     printf("Dequeuing element %d\n", dequeue(&q));
     printf("Dequeuing element %d\n", dequeue(&q));
-    printf("Dequeuing element %d\n", dequeue(&q));
-    enqueue(&q, 45);
-    enqueue(&q, 45);
-    enqueue(&q, 45);
  
     if(isEmpty(&q)){
         printf("Queue is empty\n");
